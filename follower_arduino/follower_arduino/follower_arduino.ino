8cf4a99a6beca39705ce64ab9bf541673dd71a50
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2022-11-05 16:23:35 -0400

    Added new file for follower arduino

---
+++ follower_arduino/follower_arduino/follower_arduino.ino
@@ -0,0 +1,35 @@
+
+/*****************************PIN DECLARATIONS*****************************/
+//this is the code for the follower arduino
+#include <Wire.h>
+
+#define SENSORPIN 0; //an example
+
+
+
+
+/*****************************GLOBAL VARIABLES*****************************/
+
+
+
+// follower arduino accepts a sequence from the leader arduino 
+
+/************************HELPER FUNCTION PROTOTYPES************************/
+void check_for_message();
+/**********************HELPER FUNCTION IMPLEMENTATIONS*********************/
+
+void check_for_message() {
+  
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  check_for_message();
+}
