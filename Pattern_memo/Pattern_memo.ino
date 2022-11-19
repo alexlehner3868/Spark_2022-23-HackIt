152cbf15b7542d94414ac2c561ed12997d29ecbf
gin-guo <ginnyguoshi@gmail.com>
2022-11-19 16:40:54 -0500

    Update Pattern_memo.ino

--- Pattern_memo/Pattern_memo.ino
+++ Pattern_memo/Pattern_memo.ino
@@ -8,21 +8,23 @@
 
 
 /*****************************GLOBAL VARIABLES*****************************/
-
+int level = 0;
 
 
 
 /************************HELPER FUNCTION PROTOTYPES************************/
-void waitToStartGame(); // wait and do nothing until 
-long long createRandomPattern(int size){ // output binary n-by-n matrix for pattern
-  int grid_length = (size/3)+2;
+void beginPattern(); // light up all LEDs within size of the level dimensions to indicate the grid size the pattern will be created in
+
+
+long long createRandomPattern(int level){ // output binary n-by-n matrix for pattern
+  int grid_length = (level/3)+2;
   int grid_size = grid_length*grid_length;
   bool marked[grid_size];
   for(int i = 0;i<grid_size;i++){
     marked[i] = false;
   }
   long long pattern = 0; //Bitmask that represents the state of the grid
-  for(int i = 0;i<size;i++){
+  for(int i = 0;i<level;i++){
     int next = random(0, grid_size);
     while(marked[next]==true){ //If bit has already been picked, pick a new bit
       next = random(0, grid_size);
@@ -42,21 +44,25 @@ long long createRandomPattern(int size){ // output binary n-by-n matrix for patt
   */
   return pattern;
 }
-void updateLights(int pattern); //
-bool checkPattern(long long input_pattern, long long pattern){ // check if inputted Pattern matches the one given
+
+
+void updateLights(int pattern); // light up the board based on pattern given – sustains pattern for 3 seconds
+
+
+
+void readInput(); // read the user input, light up the correspond LED and sustain it
+                  //    output the corresponding binary value
+
+
+
+bool checkPattern(long long input_pattern, long long pattern){ // check if inputted values is 
 	return input_pattern==pattern;
 }
-void errorResponse(); // change LED to RED [& buzz motor] if user input is incorrect
 
 
-void MAXColourGeneration(); //[output n-by-n matrix with RGB specifications]
-void MAXupdateLights(int MAXpattern);
-void MAXcheckColour();
-void setScore(); // [we don't need to worry about this yet] calculate round score
-void checkTimeLimit(); //[if time is up, output errorResponse]
-
-/**********************HELPER FUNCTION IMPLEMENTATIONS*********************/
-
+void errorResponse(); // if user input is incorrect (checkPattern returns False), output "X" pattern on entire grid
+void setScore(); // calculate and output arbitrary score (+1 if won, +0 if lost)
+void checkTimeLimit(); //if time is up, output errorResponse (incorporate global timer that starts whenever output pattern is removed)
 
 
 
@@ -70,5 +76,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  level += 1;
 
 }
