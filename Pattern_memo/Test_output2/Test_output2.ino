bcf39b808d0a59285e808fb8e536aaaa46618339
gin-guo <72530527+gin-guo@users.noreply.github.com>
2023-02-11 17:09:28 -0500

    Update Test_output2.ino

--- Pattern_memo/Test_output2/Test_output2.ino
+++ Pattern_memo/Test_output2/Test_output2.ino
@@ -1,27 +1,37 @@
 //TEST UPDATELIGHTS FUNCTION
 
 /************global variables*************/
+//led output variables
+int latchPin = 5;  // latch pin of shift register
+int clockPin = 6; // clock pin of shift register
+int dataPin = 4;  // data pin of shift register
+
 byte leds1 = 0;   // each var controls leds in each shift register
 byte leds2 = 0; 
 
-int pattern = 
+  int pattern1[] = {0, 1, 2, 3};
+  int pattern2[] = {0, 1, 2, 3};
 
 /************function*************/
-void updateLights(int pattern){
+void updateShiftRegister(){
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, leds2);
+   shiftOut(dataPin, clockPin, LSBFIRST, leds1);
+   digitalWrite(latchPin, HIGH);
+}
+
+void updateLights(int pattern1, int pattern2){
   leds1 = 0;  // initialize leds
   leds2 = 0;
   updateShiftRegister();
 
-  int pattern1 = pattern[:8];
-  int pattern2 = pattern[8:];
-
-  for (int i = 0; i < sizeof(pattern1)/sizeof(pattern1[0]); i++){
-    bitSet(leds1, pattern1[i]);
+  for (int i = 0; i < sizeof(pattern1)/sizeof(pattern1); i++){
+    bitSet(leds1, pattern1);
     updateShiftRegister();
   }
   
-   for (int j = 0; j < sizeof(pattern2)/sizeof(pattern2[0]); j++){
-    bitSet(leds2, pattern2[j]);
+   for (int j = 0; j < sizeof(pattern2)/sizeof(pattern2); j++){
+    bitSet(leds2, pattern2);
     updateShiftRegister();
   }
 
